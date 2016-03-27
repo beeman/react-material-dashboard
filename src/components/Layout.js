27400cb71eaccbcf6db94d05864555833bb0918a
micrum <rumiancau@gmail.com>
2016-03-27 16:30:48 +0300

    material-ui version bumped, added link to github repo, material icons added, minor styles adjustments provided

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,16 @@ import AppBar from 'material-ui/lib/app-bar';
 import LeftNav from 'material-ui/lib/left-nav';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import { Link } from 'react-router';
+import IconButton from 'material-ui/lib/icon-button';
+
+const githubButton = (
+  <IconButton
+    iconClassName="material-icons"
+    href="https://github.com/micrum/react-material-dashboard"
+    linkButton={true}>
+    code
+  </IconButton>
+);
 
 class Layout extends React.Component {
   constructor(props) {
@@ -24,20 +34,23 @@ class Layout extends React.Component {
         <AppBar
           title="React Material Dashboard"
           onLeftIconButtonTouchTap={this.toggleNavigation}
+          iconElementRight={githubButton}
         />
         <LeftNav
           open={this.state.open}
           docked={false}
           onRequestChange={(open) => this.setState({open})}
         >
-          <Link to="/home" onTouchTap={this.toggleNavigation}>
+          <Link to="/home" onTouchTap={this.toggleNavigation} className="nav-link">
             <MenuItem>Home</MenuItem>
           </Link>
-          <Link to="/table" onTouchTap={this.toggleNavigation}>
+          <Link to="/table" onTouchTap={this.toggleNavigation} className="nav-link">
             <MenuItem>Table</MenuItem>
           </Link>
         </LeftNav>
-        {this.props.children}
+        <div className="page-content">
+          {this.props.children}
+        </div>
       </div>
     );
   }
